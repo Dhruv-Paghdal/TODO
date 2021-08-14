17d1fb5d1d6cb98a0794ec91526f9bf6256031c0
Dhruv-Paghdal <85886757+Dhruv-Paghdal@users.noreply.github.com>
2021-08-14 11:49:24 +0530

    Update DELETE_SF.php

--- DELETE_SF.php
+++ DELETE_SF.php
@@ -1,7 +1,7 @@
 <?php
      $ITEM=$_GET["item"];
-     $CON=mysqli_connect("localhost","root","","todo");
-     $QUERY='delete from todo where ITEM="'.$ITEM.'"';
+     $CON=mysqli_connect("localhost","root","","database_name");
+     $QUERY='delete from table_name where ITEM="'.$ITEM.'"';
      mysqli_query($CON,$QUERY);
      header("location: TODO.php");
 ?>
