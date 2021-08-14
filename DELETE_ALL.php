8d22d2e6a9bc1103680d31bd5ee0dfa8454e6679
Dhruv-Paghdal <85886757+Dhruv-Paghdal@users.noreply.github.com>
2021-08-14 11:48:42 +0530

    Update DELETE_ALL.php

--- DELETE_ALL.php
+++ DELETE_ALL.php
@@ -1,6 +1,6 @@
 <?php
-     $CON=mysqli_connect("localhost","root","","todo");
-     $QUERY="delete from todo";
+     $CON=mysqli_connect("localhost","root","","database_name");
+     $QUERY="delete from table_name";
      mysqli_query($CON,$QUERY);
      header("location: TODO.php");
 ?>
